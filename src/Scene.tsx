5f363d7a4bf4070e52f72f674cf856de513a86b4
jsadden8@gmail.com <support@swiftcontinental.com>
2022-10-27 15:41:39 -0400

    add floating boxes

--- src/Scene.tsx
+++ src/Scene.tsx
@@ -2,6 +2,7 @@ import React, { useRef } from 'react';
 import './App.css';
 import { Stars } from '@react-three/drei';
 import { useFrame } from '@react-three/fiber';
+import { IconBox } from './components/IconBox';
 
 export const Scene = () => {
   const pointLightRef = useRef<THREE.PointLight>(null!);
@@ -17,8 +18,52 @@ export const Scene = () => {
   return (
     <>
       <ambientLight ref={ambientLightRef} intensity={0.1} />
-      <pointLight ref={pointLightRef} intensity={10} position={[0, 0, 400]} />
+      <pointLight ref={pointLightRef} intensity={5} position={[-100, 0, 150]} />
       <Stars radius={200} />
+
+      <>
+        <IconBox position={{ x: 7, y: -30, z: -30 }} color={'purple'} />
+        <IconBox position={{ x: 0, y: -35, z: -30 }} color={'red'} />
+        <IconBox position={{ x: 10, y: -40, z: -30 }} color={'yellow'} />
+        <IconBox position={{ x: 20, y: -35, z: -30 }} color={'orange'} />
+        <IconBox position={{ x: 25, y: -30, z: -30 }} color={'blue'} />
+        <IconBox position={{ x: 35, y: -35, z: -30 }} color={'green'} />
+      </>
+
+      <>
+        <IconBox position={{ x: -35, y: -70, z: -30 }} color={'yellow'} />
+        <IconBox position={{ x: -15, y: -70, z: -30 }} color={'orange'} />
+        <IconBox position={{ x: -5, y: -80, z: -30 }} color={'red'} />
+        <IconBox position={{ x: -20, y: -75, z: -30 }} color={'purple'} />
+        <IconBox position={{ x: -25, y: -80, z: -30 }} color={'green'} />
+        <IconBox position={{ x: -40, y: -80, z: -30 }} color={'blue'} />
+      </>
+
+      <>
+        <IconBox position={{ x: 7, y: -105, z: -30 }} color={'purple'} />
+        <IconBox position={{ x: 0, y: -110, z: -30 }} color={'red'} />
+        <IconBox position={{ x: 10, y: -115, z: -30 }} color={'yellow'} />
+        <IconBox position={{ x: 20, y: -120, z: -30 }} color={'orange'} />
+        <IconBox position={{ x: 25, y: -115, z: -30 }} color={'blue'} />
+        <IconBox position={{ x: 35, y: -110, z: -30 }} color={'green'} />
+      </>
+
+      <>
+        <IconBox position={{ x: -35, y: -150, z: -30 }} color={'yellow'} />
+        <IconBox position={{ x: -15, y: -150, z: -30 }} color={'orange'} />
+        <IconBox position={{ x: -5, y: -150, z: -30 }} color={'red'} />
+        <IconBox position={{ x: -20, y: -155, z: -30 }} color={'purple'} />
+        <IconBox position={{ x: -25, y: -160, z: -30 }} color={'green'} />
+        <IconBox position={{ x: -40, y: -155, z: -30 }} color={'blue'} />
+      </>
+      <>
+        <IconBox position={{ x: 7, y: -185, z: -30 }} color={'purple'} />
+        <IconBox position={{ x: 0, y: -180, z: -30 }} color={'red'} />
+        <IconBox position={{ x: 10, y: -175, z: -30 }} color={'yellow'} />
+        <IconBox position={{ x: 20, y: -185, z: -30 }} color={'orange'} />
+        <IconBox position={{ x: 25, y: -175, z: -30 }} color={'blue'} />
+        <IconBox position={{ x: 35, y: -180, z: -30 }} color={'green'} />
+      </>
     </>
   );
 };
---
+++ src/components/IconBox/IconBox.tsx
@@ -0,0 +1,40 @@
+import React, { useRef } from 'react';
+import { useFrame } from '@react-three/fiber';
+
+interface Props {
+  position: {
+    x: number;
+    y: number;
+    z: number;
+  };
+  size?: {
+    length: number;
+    width: number;
+    height: number;
+  };
+  color: string;
+}
+
+export const IconBox = ({ position, size, color }: Props) => {
+  const mesh = useRef<THREE.Mesh>(null!);
+  const initialAngle = Math.PI * 0.25 * Math.random();
+  const randomOffset = Math.random();
+
+  // animate rocking motion
+  useFrame(() => {
+    const angleX = Math.sin(Date.now() * 0.002 + randomOffset) * Math.PI * 0.25 + initialAngle;
+    const angleY = Math.sin(Date.now() * 0.0005 + randomOffset) * Math.PI * 0.15 + initialAngle;
+    const angleZ = Math.sin(Date.now() * 0.001 + randomOffset) * Math.PI * 0.25 + initialAngle;
+
+    mesh.current.rotation.z = angleZ;
+    mesh.current.rotation.x = angleX;
+    mesh.current.rotation.y = angleY;
+  });
+
+  return (
+    <mesh ref={mesh} position={[position.x, position.y, position.z]}>
+      <boxGeometry args={[size?.length || 3, size?.width || 3, size?.height || 3]} />
+      <meshStandardMaterial color={color} />
+    </mesh>
+  );
+};
